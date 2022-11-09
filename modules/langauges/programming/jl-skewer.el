5826bccc9364b378dea9580a0d499ff6559cf6ab
James Leslie <jamesleslie@posteo.net>
2022-11-09 22:04:09 +0000

    [skewer] Add skewer module

---
+++ modules/langauges/programming/jl-skewer.el
@@ -0,0 +1,14 @@
+(use-package skewer-mode
+  :defer t
+  :hook ((js2-mode rjsx-mode) . skewer-mode)
+  :general
+  (jl/major-modes
+    :keymaps 'skewer-mode-map
+    :states '(normal visual operator)
+    ;; :major-modes t
+    "s"  '(:ignore t :which-key "skewer")
+    "sr" #'skewer-repl
+    "sl" #'skewer-load-buffer
+    "ss" #'run-skewer
+    "sd" #'skewer-eval-defun
+    "se" #'skewer-eval-last-expression))
