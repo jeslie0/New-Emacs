ebb8299f01b6f121a9338a47bc2ef10d453c6df9
James Leslie <jamesleslie@posteo.net>
2022-11-11 11:13:11 +0000

    Improve rjsx keybindings

--- modules/langauges/programming/jl-js-doc.el
+++ modules/langauges/programming/jl-js-doc.el
@@ -2,8 +2,9 @@
   :after (:any js-mode rjsx-mode)
   :general
   (jl/major-modes
-    :keymaps '(js-mode rjsx-mode)
+    :keymaps '(js-mode-map rjsx-mode-map)
     :states '(normal visual operator)
     :major-modes t
+    "i" '(:ignore t :which-key "insert")
     "if" 'js-doc-insert-function-doc
     "it" 'js-doc-insert-tag))
--- modules/langauges/programming/jl-rjsx.el
+++ modules/langauges/programming/jl-rjsx.el
@@ -11,13 +11,16 @@
   (setq sgml-basic-offset 4))
 
 (use-package xref-js2
+  :hook (js2-mode . (lambda () (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
   :general
   (jl/major-modes
     :keymaps 'rjsx-mode-map
     :states '(normal visual operator)
     :major-modes t
-    "gG" 'xref-find-definitions)
+    "g"  '(:ignore t :which-key "goto")
+    "gg" 'xref-find-definitions
+    "gr" 'xref-find-references
+    "ga" 'xref-find-apropos)
   :config
   (setq xref-js2-search-program 'rg))
-(add-hook 'js2-mode-hook (lambda ()
-                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
