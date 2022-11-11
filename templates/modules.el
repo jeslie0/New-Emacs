a6bcc5bb76ccd8220b9a2432f05d8802b86d93e9
James Leslie <jamesleslie@posteo.net>
2022-11-11 17:46:35 +0000

    Change default modules file to a template

---
+++ templates/modules.el
@@ -0,0 +1,155 @@
+(setq user-modules '(jl-straight
+                     jl-misc
+                     jl-cleaning
+                     jl-keybindings
+                     jl-UIconfiguration
+                     ;;; Completion frameworks
+                     ;; jl-ivy
+                     jl-vertico
+                     ;;; Project Management
+                     ;; jl-projectile
+                     jl-project
+                     jl-magit
+                     ;; jl-magit-forge
+                     ;; jl-git-gutter
+                     jl-diff-hl
+                     jl-vc
+                     jl-smerge
+                     jl-treemacs
+                     ;; jl-treemacs-projectile
+                     jl-treemacs-magit
+                     jl-treemacs-perspective
+                     jl-helpful
+                     jl-eldoc
+                     jl-docker
+
+
+                     jl-dashboard
+
+                     jl-rainbow-delimiters
+                     jl-outshine
+                     jl-aggressive-indent
+                     jl-golden-ratio
+                     jl-highlight-indent-guides
+                     jl-page-break-lines
+                     jl-smartparens
+                     jl-saveplace
+                     jl-variable-pitch
+                     jl-visual-line
+                     jl-hl-todo
+                     jl-polymode
+
+                     ;;; Passwords
+                     jl-pinentry
+                     jl-pass
+                     ;; jl-ivy-pass
+
+                     ;;; Languages
+                     ;;;; Tools
+                     jl-lsp
+                     ;; jl-lsp-ivy
+                     jl-eglot
+                     jl-tree-sitter
+
+                     ;;;; Proof assistants
+                     jl-agda
+                     ;; jl-org-agda
+                     jl-coq
+
+                     ;;;; Programming
+                     jl-cc
+                     ;; jl-rtags
+                     jl-csharp
+                     jl-elm
+                     jl-haskell
+                     jl-lisps
+                     ;; jl-evil-lisp-state
+                     jl-shells
+                     ;; jl-insert-shebang
+                     jl-nix
+                     ;; jl-java
+                     jl-js
+                     jl-rjsx
+                     jl-skewer
+
+                     jl-js-doc
+                     jl-python
+
+                     ;;;; Text
+                     jl-org
+                     ;; jl-ob-async
+                     ;; jl-org-protocol
+                     ;; jl-ox-altacv
+                     jl-ox-extra
+                     ;; jl-org-projectile
+
+                     jl-auctex
+                     jl-bibtex
+                     ;; jl-ivy-bibtex
+                     jl-org-ref
+                     jl-markdown
+
+                     ;;;; Other
+                     jl-yaml
+                     jl-protobuf
+                     jl-json
+
+                     ;;; Readers
+                     jl-elfeed
+                     ;; jl-elfeed-tube
+                     ;; jl-elfeed-goodies
+                     jl-elpher
+                     jl-erc
+                     ;; jl-erc-image
+                     jl-ement
+                     jl-eww
+                     jl-mu4e
+                     ;; jl-mu4e-alert
+                     jl-mpv
+
+                     ;;;
+                     jl-avy
+
+                     ;;; Spelling & correction
+                     jl-ispell
+                     jl-flyspell
+                     jl-consult-flyspell
+                     jl-flyspell-correct
+                     ;; jl-spell-fu
+                     ;; jl-flycheck
+                     jl-flymake
+
+                     ;;; Auto completions
+                     jl-yasnippet
+                     jl-corfu
+                     jl-corfu-doc
+                     jl-cape
+                     ;; jl-company
+
+                     ;;; Tools
+                     jl-perspective
+                     jl-undo-tree
+                     jl-pandoc
+                     jl-iedit
+                     jl-simple-httpd
+                     jl-define-word
+                     jl-floobits
+                     jl-edit-server
+                     jl-esup
+                     ;; jl-openwith
+                     jl-sudo-edit
+                     ;; jl-artist
+                     jl-pdf-tools
+                     ;; jl-direnv
+                     jl-envrc
+
+                     ;;; Shells
+                     jl-term
+                     jl-vterm
+                     jl-eshell
+
+                     ;;; Fun
+                     ;; jl-snow
+                     ;; jl-fireplace
+                     ;; jl-nyan
+                     ))
