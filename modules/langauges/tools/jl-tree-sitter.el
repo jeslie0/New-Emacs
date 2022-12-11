59f755b1be9ff8760db4269cba7f350daf2d02d6
James Leslie <jamesleslie@posteo.net>
2022-12-11 18:31:46 +0000

    [treesitter] Use tree-sitter where possible

--- modules/langauges/tools/jl-tree-sitter.el
+++ modules/langauges/tools/jl-tree-sitter.el
@@ -1,12 +1,19 @@
 (use-package tree-sitter
   :defer t
+  :hook (prog-mode . tree-sitter-mode)
+  :init (global-tree-sitter-mode)
   :config
   (add-to-list 'tree-sitter-major-mode-language-alist '(haskell-mode . haskell))
   (add-to-list 'tree-sitter-major-mode-language-alist '(agda2-mode . agda)))
 
 (use-package tree-sitter-langs
   :defer t
-  :after tree-sitter)
+  :after tree-sitter
+  :config
+  (progn
+    (let* ((use-tree-sitter-langs  (mapcar 'car tree-sitter-major-mode-language-alist))
+           (tree-sitter-mode-hooks  (mapcar #'(lambda (mode) (intern (concat (symbol-name mode) "-hook"))) use-tree-sitter-langs)))
+      (mapc #'(lambda (hook) (add-hook hook 'tree-sitter-hl-mode)) tree-sitter-mode-hooks))))
 
 (use-package evil-tree-edit
   :defer t
