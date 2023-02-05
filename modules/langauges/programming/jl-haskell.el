4a96596c1f9117b443af850af946495ee9668302
James Leslie <jamesleslie@posteo.net>
2023-02-05 22:47:07 +0000

    Refactor haskell-mode code

--- modules/langauges/programming/jl-haskell.el
+++ modules/langauges/programming/jl-haskell.el
@@ -109,33 +109,6 @@
   :config
   (jl/haskell-keys))
 
-(use-package hindent
-  :after haskell-mode
-  :general
-  (jl/major-modes
-    :states '(normal visual operator)
-    :keymaps 'haskell-mode-map
-    :major-modes t
-    "f" '(:ignore nil :which-key "format")
-    "fB" 'hindent-reformat-buffer
-    "fd" 'hindent-reformat-decl
-    "ff" 'hindent-reformat-decl-or-fill
-    "fr" 'hindent-reformat-region)
-  :hook (haskell-mode . hindent-mode))
-
-(use-package hlint-refactor
-  :hook (haskell-mode . hlint-refactor-mode)
-  :general
-  (jl/major-modes
-    :states '(normal visual)
-    :keymaps 'haskell-mode-map
-    :major-modes t
-    "rb" 'hlint-refactor-refactor-buffer
-    "rr" 'hlint-refactor-refactor-at-point))
-
-(use-package cmm-mode
-  :defer t)
-
 (use-package lsp-haskell
   :defer t
   :custom
