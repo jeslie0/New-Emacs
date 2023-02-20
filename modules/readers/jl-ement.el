a5fb5ec2bdbcdfca3d95f4b3efd9efe19a49443d
James Leslie <jamesleslie@posteo.net>
2023-02-20 21:03:00 +0000

    [Ement] Remove straight commands from ement

--- modules/readers/jl-ement.el
+++ modules/readers/jl-ement.el
@@ -1,10 +1,5 @@
-(use-package plz
-  :defer t
- :straight (:host github :repo "alphapapa/plz.el" :branch "master" :files ("*.el")))
-
 (use-package ement
   :defer t
-  :straight (:host github :repo "alphapapa/ement.el" :branch "master" :files ("*.el"))
   :commands (ement-connect)
   :general
   (jl/SPC-keys
