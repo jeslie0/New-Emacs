c83b7cacee35239db1cf08eb8d6b7db07ca511c5
James Leslie <jamesleslie@posteo.net>
2023-07-27 19:40:07 +0100

    Add "," to vc-log-mode

--- modules/projectmanagement/jl-vc.el
+++ modules/projectmanagement/jl-vc.el
@@ -3,6 +3,13 @@
   (jl/SPC-keys
     :prefix-map 'vc-prefix-map
     :prefix "SPC g v")
+  ;; Doesn't work
+  ;; (jl/major-modes
+  ;;   :keymap 'log-edit-mode-map
+  ;;   :states '(normal visual operator)
+  ;;   :major-modes t
+  ;;   "," #'log-edit-done
+  ;;   "a" #'log-edit-kill-buffer)
   (jl/SPC-keys
     "gv" '(:ignore t :which-key "VC")
     "gvM" '(:ignore t :which-key "Mergebase")))
