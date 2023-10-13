283e220c8a399c8c7ee19edecebf1e39761e3f38
James Leslie <jamesleslie@posteo.net>
2023-10-13 22:59:49 +0100

    Add PSCI to purescript

--- modules/langauges/programming/jl-purescript.el
+++ modules/langauges/programming/jl-purescript.el
@@ -4,3 +4,15 @@
 
 (use-package dhall-mode
   :defer t)
+
+(use-package psci
+  :straight (:host github :repo "jeslie0/emacs-psci" :branch "master" :files ("psci.el"))
+  :defer t
+  :general
+  (jl/major-modes
+    :keymaps 'purescript-mode-map
+    :states '(normal visual operator)
+    :major-modes t
+    "s" '(:ignore t :which-key "repl")
+    "sr" 'psci
+    "sb" 'psci/load-module!))
